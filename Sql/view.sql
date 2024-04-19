40bd596f7ae30530d6892840d41d5bd855b4542f
Alessia65 <alessia.disabato@studio.unibo.it>
2024-04-19 16:08:27 +0200

    view nr 2

--- Sql/view.sql
+++ Sql/view.sql
@@ -1,25 +1,11 @@
 USE ESQL;
 
-DROP VIEW IF EXISTS ClassificaQuesitiPerRisposte;
-DROP VIEW IF EXISTS classificaTestCompletati;
 DROP VIEW IF EXISTS classifica_test_completati;
-DROP VIEW IF EXISTS classifica_studenti;
+DROP VIEW IF EXISTS classifica_risposte_corrette;
 DROP VIEW IF EXISTS classifica_quesiti;
 
 -- VIEW
-
-
-CREATE VIEW ClassificaQuesitiPerRisposte AS
-SELECT  QUESITO.NumeroProgressivo,QUESITO.TitoloTest,COUNT(RC.NumeroProgressivoCompletamento) + COUNT(RCC.NumeroProgressivoCompletamento) AS NumeroTotaleRisposte
-FROM QUESITO 
-JOIN RISPOSTAQUESITORISPOSTACHIUSA AS RC ON QUESITO.NumeroProgressivo = RC.NumeroProgressivoQuesito AND QUESITO.TitoloTest = RC.TitoloTest
-JOIN RISPOSTAQUESITOCODICE AS RCC ON QUESITO.NumeroProgressivo = RCC.NumeroProgressivoQuesito AND QUESITO.TitoloTest = RCC.TitoloTest
-GROUP BY QUESITO.NumeroProgressivo, QUESITO.TitoloTest
-ORDER BY NumeroTotaleRisposte DESC;
-
-
-        
-
+-- view nr 1
 CREATE VIEW classifica_test_completati AS
 	SELECT s.CodiceAlfaNumerico, COUNT(*) AS numero_test_completati
 	FROM STUDENTE AS s LEFT JOIN COMPLETAMENTO AS c ON s.Email = c.EmailStudente
@@ -27,18 +13,25 @@ CREATE VIEW classifica_test_completati AS
 	GROUP BY s.CodiceAlfaNumerico;
         
 
-# QUI BISOGNA LAVORARCI, VANNO UNITE LE DUE, NON POSSONO ESSERE SEPARATE
-CREATE VIEW classifica_studenti AS
-	SELECT s.CodiceAlfaNumerico, 
-    ROUND((COUNT(CASE WHEN rc.Esito = TRUE THEN 1 ELSE 0 END) / COUNT(rc.NumeroProgressivoCompletamento)) * 100, 2) AS percentuale_risposte_chiuse_corrette,
-    ROUND((COUNT(CASE WHEN rc.Esito = TRUE THEN 1 ELSE 0 END) / COUNT(rcc.NumeroProgressivoCompletamento)) * 100, 2) AS percentuale_risposte_codice_corrette
-	FROM STUDENTE AS s 
-    LEFT JOIN REALIZZAZIONE AS rz ON s.Email = rz.EmailStudente
-	LEFT JOIN RISPOSTAQUESITORISPOSTACHIUSA AS rc ON rz.NumeroProgressivoCompletamento = rc.NumeroProgressivoCompletamento
-    LEFT JOIN RISPOSTAQUESITORISPOSTACHIUSA AS rcc ON rz.NumeroProgressivoCompletamento = rcc.NumeroProgressivoCompletamento
-	GROUP BY s.CodiceAlfaNumerico;
-        
+-- view nr 2
+CREATE VIEW classifica_risposte_corrette AS
+	SELECT
+		C.EmailStudente AS codiceStudente,
+		IFNULL(((SUM(RQC.Esito) + SUM(RCC.Esito)) / (COUNT(RQC.NumeroProgressivoCompletamento) + COUNT(RCC.NumeroProgressivoCompletamento))) * 100, 0) AS percentualeRisposteCorrette
+	FROM
+		COMPLETAMENTO C
+	LEFT JOIN
+		RISPOSTAQUESITORISPOSTACHIUSA AS RQC ON C.NumeroProgressivo = RQC.NumeroProgressivoCompletamento
+	LEFT JOIN
+		RISPOSTAQUESITOCODICE AS RCC ON C.NumeroProgressivo = RCC.NumeroProgressivoCompletamento
+	GROUP BY
+		C.EmailStudente
+	ORDER BY
+		percentualeRisposteCorrette DESC;
 
+
+
+-- view nr 3
 CREATE VIEW classifica_quesiti AS
 	SELECT q.NumeroProgressivo, q.TitoloTest, COUNT(r.NumeroProgressivoCompletamento) AS num_risposte_codice_inserite, 
 		COUNT(rc.NumeroProgressivoCompletamento) AS num_risposte_chiuse_inserite
